eaa5375403918dcd5e7c2036956fe7ee5c299e0a
Kevan Hashemi <hashemi@opensourceinstruments.com>
2023-04-02 23:09:38 -0400

    Larger console image in p.pas

---
+++ Build/p.pas
@@ -0,0 +1,64 @@
+program p;
+
+{$MODESWITCH CLASSICPROCVARS+}
+{$LONGSTRINGS ON}
+
+{
+	Console-Only Test Progam for SCAM Routines. Place in LWDAQ/Build and type
+	"make p" then "./p.exe" to run.
+}
+
+uses
+	scam,bcam,utils,images,image_manip;
+	
+const
+	width=80;
+	height=40;
+
+var
+	sc:bcam_camera_type;
+	q,qq:xy_point_type;
+	r:xyz_point_type;
+	line:xyz_line_type;
+	sphere:xyz_sphere_type;
+	cylinder:xyz_cylinder_type;
+	d:real;
+	i,j,k:integer;
+	ip:image_ptr_type;
+	i_ext,j_ext:integer;
+	
+begin
+	writeln('Testing projection, mount +x left, mount +y up in view.');
+	ip:=new_image(height,width);
+	with ip^.analysis_bounds do begin
+		top:=0;
+	end;
+	clear_overlay(ip);
+	sc:=bcam_camera_from_string('scam 0 0 0 0 0 3 25 0');
+	bcam_generic_pixel_um:=100;
+	bcam_generic_center_x:=width*0.5*bcam_generic_pixel_um/um_per_mm;
+	bcam_generic_center_y:=height*0.5*bcam_generic_pixel_um/um_per_mm;
+
+	sphere:=xyz_sphere_from_string('50 20 1000 20');
+	scam_project_sphere(ip,sphere,sc);
+	
+	cylinder:=xyz_cylinder_from_string('0 0 2000 -0.01 0.01 -0.8 10 1500');
+	scam_project_cylinder(ip,cylinder,sc);
+
+	write(' ');
+	for i:=0 to ip^.i_size-1 do write('--');
+	writeln;
+	for j:=0 to ip^.j_size-1 do begin
+		write('|');
+		for i:=0 to ip^.i_size-1 do begin
+			if get_ov(ip,j,i)=green_color then write('SS')
+			else if get_ov(ip,j,i)=blue_color then write('CC')
+			else write('  ');
+		end;
+		writeln('|');;
+	end;
+	write(' ');
+	for i:=0 to ip^.i_size-1 do write('--');
+	writeln;
+	
+end.
