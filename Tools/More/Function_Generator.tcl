a02d4640fae8f83ee5faad4869cab2c726eb5ecb
NathanSayer <sayer@opensourceinstruments.com>
2024-04-19 16:48:26 -0400

    FG_tool_working

--- Tools/More/Function_Generator.tcl
+++ Tools/More/Function_Generator.tcl
@@ -10,15 +10,17 @@ proc Function_Generator_init {} {
 	LWDAQ_tool_init "Function_Generator" "1.1"
 	if {[winfo exists $info(window)]} {return ""}
 
-	package require LWFG
+	set info(data) [list]
+	set info(start_time) "0"
+	set info(channel_names) "1"
 
 	set info(control) "Idle"
 	set config(verbose) "0"
 	set config(logarithmic) "0"
 
 	set config(gen_ip) "10.0.0.37"
-	set config(gen_ch) "1"
-
+	set config(gen_ch) [lindex $info(channel_names) 0]
+	
 	set config(waveform_type) "sine"
 	set config(waveform_amplitude) "10"
 	set config(waveform_offset) "0"
@@ -30,9 +32,6 @@ proc Function_Generator_init {} {
 
 	set config(label_color) "green"
 
-	set info(data) [list]
-	set info(start_time) "0"
-
 	if {[file exists $info(settings_file_name)]} {
 		uplevel #0 [list source $info(settings_file_name)]
 	}
@@ -43,7 +42,6 @@ proc Function_Generator_init {} {
 proc Function_Generator_on {} {
 	upvar #0 Function_Generator_info info 
 	upvar #0 Function_Generator_config config 
-	global LWFG
 
 	LWDAQ_print $info(text) "Channel $config(gen_ch), $config(waveform_type),\
 		$config(waveform_frequency) Hz,\
@@ -70,24 +68,14 @@ proc Function_Generator_on {} {
 	}
 }
 
-proc Function_Generator_off {} {
-	upvar #0 Function_Generator_info info 
-	upvar #0 Function_Generator_config config 
+# LWFG_configure configures a function generator for continuous generation of a square,
+# triangle, or sine wave. We specify an IP address and channel number. We give the 
+# frequency and the low and high voltages of the waveform.
+#
+proc LWFG_configure {ip ch_num waveform frequency v_lo v_hi} {
 
-	LWDAQ_print $info(text) "Channel $config(gen_ch), 0 V amplitude, 0 V offset." purple
-	set result [LWFG_off $config(gen_ip) $config(gen_ch)]
-	if {[LWDAQ_is_error_result $result]} {
-		LWDAQ_print $info(text) $result
-	}
-	
-	return ""
-}
-	
-	
-proc sweep_off {} {
-	upvar #0 Function_Generator_info info 
-	upvar #0 Function_Generator_config config 
-
+	# Determine the lower and upper DAC values for our lower and upper waveform
+	# voltages.
 	set LWFG(data_portal) "63"
 	set LWFG(ch1_ram) "0x0000"
 	set LWFG(ch1_rc) "0x8000"
@@ -105,30 +93,211 @@ proc sweep_off {} {
 	set LWFG(div_min) "2"
 
 	set LWFG(ch_cnt_lo) "0"
+	set LWFG(ch_cnt_z) "128"
 	set LWFG(ch_cnt_hi) "255"
 	set LWFG(ch_v_lo) "-10.0"
 	set LWFG(ch_v_hi) "+10.0"
 
-	set LWFG(rc_options) "5.1e1 0x11 4.0e2 0x21 3.0e3 0x41 \
-		2.3e4 0x81 1.7e5 0x82 1.3e6 0x48 9.8e6 0x88"
+	set LWFG(rc_options) "1.3e1 0x01 5.1e1 0x11 1.1e2 0x21 2.7e2 0x14 5.6e2 0x18 1.1e3 0x21 \
+		2.4e3 0x22 5.9e3 0x24 1.2e4 0x28 2.6e4 0x41 5.5e4 0x42 1.4e5 0x44 \
+		2.8e5 0x48 1.0e6 0x81 2.2e6 0x82 5.4e6 0x84 1.1e7 0x88"
 	set LWFG(rc_fraction) "0.01"
+	set LWFG(rc_default) "0x11"
+	set lsb [expr ($LWFG(ch_v_hi) - $LWFG(ch_v_lo)) \
+		/ ($LWFG(ch_cnt_hi) - $LWFG(ch_cnt_lo))]
+	set dac_lo [expr round(($v_lo - $LWFG(ch_v_lo)) / $lsb)]
+	set dac_hi [expr round(($v_hi - $LWFG(ch_v_lo)) / $lsb)]
 
-	set v_lo [expr $config(waveform_offset) - $config(waveform_amplitude)]
-	set v_hi [expr $config(waveform_offset) + $config(waveform_amplitude)]
-	set waveform $config(waveform_type)
-	set sweep_period $config(sweep_time)
-	set ch_num $config(gen_ch)
-	set ip $config(gen_ip)
+	# We begin by getting getting the number of points we need for one period
+	# with the fastest sample rate, and if num_pts is too large, we increase
+	# the divisor until num_pts is small enough.
+	set divisor $LWFG(div_min)
+	set num_pts [expr round($LWFG(clock_hz) / $frequency / $divisor)]
+	while {$num_pts > $LWFG(max_pts)} {
+		incr divisor
+		set num_pts [expr round($LWFG(clock_hz) / $frequency / $divisor)]
+	}
+	
+	# If num_pts is too small, we increase the number of cycles in the waveform
+	# and recalculate our number of points until it is large enough.
+	set num_cycles 1
+	while {$num_pts < $LWFG(min_pts)} {
+		incr num_cycles
+		set num_pts [expr round($LWFG(clock_hz) * $num_cycles / $frequency / $divisor)]
+	}
 
+	# Calculate the actual frequency we are going to get.
+	set actual [format %.3f [expr $LWFG(clock_hz)*$num_cycles/$num_pts/$divisor]]
+	
+	# Generate the waveform.
+	set values [list]
+	set period [expr 1.0*$num_pts/$num_cycles]
+	switch $waveform {
+		"sine" {
+			set pi 3.141592654
+			for {set i 0} {$i < $num_pts} {incr i} {
+				set phase [expr fmod($i,$period)/$period]
+				lappend values [expr $dac_lo + \
+					round(($dac_hi-$dac_lo)*0.5*(1.0+sin(2*$pi*$phase)))]
+			}
+		}
+		"square" {
+			for {set i 0} {$i < $num_pts} {incr i} {\
+				set phase [expr fmod($i,$period)/$period]
+				if {$phase <= 0.5} { 
+					lappend values $dac_hi
+				} else {
+					lappend values $dac_lo
+				}
+			}
+		}
+		"triangle" {
+			for {set i 0} {$i < $num_pts} {incr i} {\
+				set phase [expr fmod($i,$period)/$period]
+				if {$phase <= 0.5} { 
+					lappend values [expr $dac_lo + \
+						round(($dac_hi-$dac_lo)*$phase*2.0)]
+				} else {
+					lappend values [expr $dac_hi - \
+						round(($dac_hi-$dac_lo)*($phase-0.5)*2.0)]
+				}
+			}
+		}
+		default {
+			return "ERROR: Unkown waveform \"$waveform\"."
+		}
+	}
+	
+	# Limit the values to the range 0 to 255.
+	set clipped_values [list]
+	foreach value $values {
+		if {$value < 0} {set value 0}
+		if {$value > 255} {set value 255}
+		lappend clipped_values $value
+	}
+	set values $clipped_values
+	
+	# Choose the filter.
+	set rc [lindex $LWFG(rc_options) 0]
+	set filter [lindex $LWFG(rc_options) 1]
+	switch $waveform {
+		"sine"     - 
+		"triangle" {
+			set ideal_rc [expr 1.0E9/$frequency*$LWFG(rc_fraction)]
+			foreach {p code} $LWFG(rc_options) {
+				if {$p < $ideal_rc} {
+					set filter $code
+					set rc $p
+				}
+			}
+		}
+	}
+	
+	# Configure the function generator using TCPIP messaging.
 	if {[catch {
+	
+		# Open a socket to the function generator.
 		set sock [LWDAQ_socket_open $ip]
 		
+		# Write the waveform values to the waveform memory.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_ram)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format c* $values]
+		
+		# Set the filter configuration register.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_rc)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format c [expr $filter]]
+		
+		# Set the clock divisor.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_div)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format I [expr $divisor - 1]]
+		
+		# Set the waveform length.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_len)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format S [expr $num_pts - 1]]
+		
+		# Wait for the controller to be done with configuration.
+		set id [LWDAQ_hardware_id $sock]
+		
+		# Close the socket.
+		LWDAQ_socket_close $sock
+	} error_message]} {
+		catch {LWDAQ_socket_close $sock}
+		return "ERROR: $error_message\."
+	}
+	
+	# Return enough information about the waveform for us to assess accuracy.
+	return "$dac_lo $dac_hi $divisor $num_pts $num_cycles [format %.0f $rc]"
+}
+
+
+proc Function_Generator_off {} {
+	upvar #0 Function_Generator_info info 
+	upvar #0 Function_Generator_config config 
+
+	LWDAQ_print $info(text) "Channel $config(gen_ch), 0 V amplitude, 0 V offset." purple
+	set result [LWFG_off $config(gen_ip) $config(gen_ch)]
+	if {[LWDAQ_is_error_result $result]} {
+		LWDAQ_print $info(text) $result
+	}
+	
+	return ""
+}
+
+proc LWFG_off {ip ch_num} {
+
+	# Configure the function generator for zero output.
+	if {[catch {
+
+		# Open a socket to the function generator.
+			set LWFG(data_portal) "63"
+		set LWFG(ch1_ram) "0x0000"
+		set LWFG(ch1_rc) "0x8000"
+		set LWFG(ch1_div) "0x8002"
+		set LWFG(ch1_len) "0x800A"
+
+		set LWFG(ch2_ram) "0x4000"
+		set LWFG(ch2_rc) "0x8001"
+		set LWFG(ch2_div) "0x8006"
+		set LWFG(ch2_len) "0x800C"
+
+		set LWFG(max_pts) "8192"
+		set LWFG(min_pts) "4000"
+		set LWFG(clock_hz) "40.000e6"
+		set LWFG(div_min) "2"
+
+		set LWFG(ch_cnt_lo) "0"
+		set LWFG(ch_cnt_z) "128"
+		set LWFG(ch_cnt_hi) "255"
+		set LWFG(ch_v_lo) "-10.0"
+		set LWFG(ch_v_hi) "+10.0"
+		set LWFG(rc_default) "0x11"
+
+		set LWFG(rc_options) "1.3e1 0x01 5.1e1 0x11 1.1e2 0x21 2.7e2 0x14 5.6e2 0x18 1.1e3 0x21 \
+			2.4e3 0x22 5.9e3 0x24 1.2e4 0x28 2.6e4 0x41 5.5e4 0x42 1.4e5 0x44 \
+			2.8e5 0x48 1.0e6 0x81 2.2e6 0x82 5.4e6 0x84 1.1e7 0x88"
+		set LWFG(rc_fraction) "0.01"
+		set sock [LWDAQ_socket_open $ip]
+		
+		# Write a single zero to the waveform memory.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_ram)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format c $LWFG(ch_cnt_z)]
+		
+		# Set the filter configuration register to its default value.
+		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_rc)
+		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format c $LWFG(rc_default)]
+		
+		# Set the clock divisor to one, for which we write a zero.
 		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_div)
 		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format I 0]
+		
+		# Set the waveform length to zero.
 		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_len)
 		LWDAQ_stream_write $sock $LWFG(data_portal) [binary format S 0]
-
+		
+		# Wait for the controller to be done with configuration.
 		set id [LWDAQ_hardware_id $sock]
+		
+		# Close the socket.
 		LWDAQ_socket_close $sock
 	} error_message]} {
 		catch {LWDAQ_socket_close $sock}
@@ -137,6 +306,7 @@ proc sweep_off {} {
 
 	return ""
 }
+	
 
 proc Function_Generator_sweep {} {
 
@@ -166,12 +336,15 @@ proc Function_Generator_sweep {} {
 	set LWFG(div_min) "2"
 
 	set LWFG(ch_cnt_lo) "0"
+	set LWFG(ch_cnt_z) "128"
 	set LWFG(ch_cnt_hi) "255"
 	set LWFG(ch_v_lo) "-10.0"
 	set LWFG(ch_v_hi) "+10.0"
+	set LWFG(rc_default) "0x11"
 
-	set LWFG(rc_options) "5.1e1 0x11 4.0e2 0x21 3.0e3 0x41 \
-		2.3e4 0x81 1.7e5 0x82 1.3e6 0x48 9.8e6 0x88"
+	set LWFG(rc_options) "1.3e1 0x01 5.1e1 0x11 1.1e2 0x21 2.7e2 0x14 5.6e2 0x18 1.1e3 0x21 \
+		2.4e3 0x22 5.9e3 0x24 1.2e4 0x28 2.6e4 0x41 5.5e4 0x42 1.4e5 0x44 \
+		2.8e5 0x48 1.0e6 0x81 2.2e6 0x82 5.4e6 0x84 1.1e7 0x88"
 	set LWFG(rc_fraction) "0.01"
 
 	set v_lo [expr $config(waveform_offset) - $config(waveform_amplitude)]
@@ -220,24 +393,47 @@ proc Function_Generator_sweep {} {
 			}
 		}
 		"square" {
-			for {set i 0} {$i < $num_pts} {incr i} {\
+			set pi 3.141592654
+			for {set i 0} {$i < $num_pts} {incr i} {
 				set phase [expr fmod($i,$period)/$period]
-				if {$phase <= 0.5} { 
-					lappend values $dac_hi
+				if {$config(logarithmic) == "0"} {
+					lappend values [expr $dac_lo + \
+						($dac_hi - $dac_lo) * round(0.5 * \
+							(1.0+sin(2*$pi*$sweep_period*(($start_frequency+ \
+								(($stop_frequency-$start_frequency)/2)*$phase)*$phase))))]
 				} else {
-					lappend values $dac_lo
+					lappend values [expr $dac_lo + \
+						($dac_hi - $dac_lo) * round(0.5 * \
+							(1.0+sin(2*$pi*($start_frequency*$sweep_period* \
+								((pow(($stop_frequency/$start_frequency), ($phase)) - 1) \
+									/(log($stop_frequency/$start_frequency)))))))]
 				}
 			}
 		}
 		"triangle" {
+			set pi 3.141592654
 			for {set i 0} {$i < $num_pts} {incr i} {\
 				set phase [expr fmod($i,$period)/$period]
-				if {$phase <= 0.5} { 
-					lappend values [expr $dac_lo + \
-						round(($dac_hi-$dac_lo)*$phase*2.0)]
+				if {$config(logarithmic) == "0"} {
+					set n [expr (sin(2*$pi*$phase*$sweep_period*($stop_frequency*$phase+$start_frequency)) + \
+					 $phase)]
+					if {$phase <= $n} { 
+						lappend values [expr $dac_lo + \
+							round(($dac_hi-$dac_lo)*$phase*$sweep_period*($stop_frequency*$phase+$start_frequency))]
+					} else {
+						lappend values [expr $dac_hi - \
+							round(($dac_hi-$dac_lo)*($phase-0.5)*$sweep_period*($stop_frequency*$phase+$start_frequency))]
+					}
 				} else {
-					lappend values [expr $dac_hi - \
-						round(($dac_hi-$dac_lo)*($phase-0.5)*2.0)]
+					set n [expr (sin(2*$pi*$phase*$sweep_period*($stop_frequency*$phase+$start_frequency)) + \
+						$phase)]
+					if {$phase <= $n} { 
+						lappend values [expr $dac_lo + \
+							round(($dac_hi-$dac_lo)*$phase*$sweep_period*($stop_frequency*$phase+$start_frequency))]
+					} else {
+						lappend values [expr $dac_hi - \
+							round(($dac_hi-$dac_lo)*($phase-0.5)*$sweep_period*($stop_frequency*$phase+$start_frequency))]
+					}
 				}
 			}
 		}
@@ -265,6 +461,7 @@ proc Function_Generator_sweep {} {
 	# Open a socket to the function generator and configure it through
 	# the data portal by means of stream write instructions.
 	if {[catch {
+		# Open a socket to the function generator.
 		set sock [LWDAQ_socket_open $ip]
 		
 		LWDAQ_set_data_addr $sock $LWFG(ch$ch_num\_ram)
@@ -303,6 +500,17 @@ proc Function_Generator_open {} {
 
 	set f [frame $w.control]
 	pack $f -side top -fill x 
+
+
+	label $f.mtl -text "Channel:"
+	tk_optionMenu $f.mtm Function_Generator_config(gen_ch) 2
+	foreach s $info(channel_names) {
+		$f.mtm.menu add command -label $s \
+			-command "set Function_Generator_config(gen_ch) $s"
+	}
+	set config(gen_ch) [lindex $info(channel_names) 0]
+	pack $f.mtl $f.mtm -side left -expand 1
+
 
 	foreach a {On Off Sweep} {
 		set b [string tolower $a]
